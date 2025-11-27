94373b21ad401b914f7da878415606dd923d1705
Martin N <25070490+ngwwm@users.noreply.github.com>
2025-11-27 23:26:56 +0000

    Remove unused logo import from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
